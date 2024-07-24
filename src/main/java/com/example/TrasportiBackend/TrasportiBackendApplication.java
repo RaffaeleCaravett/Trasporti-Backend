b0a7ac98522909751890860c47934a8155582cac
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-24 16:09:11 +0200

    created project

---
+++ src/main/java/com/example/TrasportiBackend/TrasportiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrasportiBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasportiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrasportiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TrasportiBackend/TrasportiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrasportiBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrasportiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
