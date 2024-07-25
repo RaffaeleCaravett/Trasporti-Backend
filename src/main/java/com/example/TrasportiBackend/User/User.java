165fce3ad23d8cc3586f0a5f4c94616d27e2a6b9
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-25 16:34:16 +0200

    created user package

---
+++ src/main/java/com/example/TrasportiBackend/User/User.java
@@ -0,0 +1,20 @@
+package com.example.TrasportiBackend.User;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "User")
+@Getter
+@Setter
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String citta;
+    private String regione;
+    private String indirizzo;
+    private long cap;
+}
