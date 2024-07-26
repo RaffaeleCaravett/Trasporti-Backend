45d042013ab65aade911c31d65d01cced5e59f7c
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-26 21:57:22 +0200

    done put aziendaById

--- src/main/java/com/example/TrasportiBackend/User/UserService.java
+++ src/main/java/com/example/TrasportiBackend/User/UserService.java
@@ -1,7 +1,9 @@
 package com.example.TrasportiBackend.User;
 
+import com.example.TrasportiBackend.enums.Settore;
 import com.example.TrasportiBackend.exceptions.BadRequestException;
 import com.example.TrasportiBackend.exceptions.UserNotFoundException;
+import com.example.TrasportiBackend.payloads.AziendaDTO;
 import com.example.TrasportiBackend.payloads.TrasportatoreDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,7 +25,7 @@ public class UserService {
         return aziendaRepository.findById(id).orElseThrow(()-> new UserNotFoundException("Azienda con id " + id + " non trovato in db"));
     }
 
-    public Trasportatore putById(long id , TrasportatoreDTO trasportatoreDTO) {
+    public Trasportatore putTrasportatoreById(long id , TrasportatoreDTO trasportatoreDTO) {
         try {
             Trasportatore trasportatore = trasporatoreRepository.findById(id).orElseThrow(() -> new UserNotFoundException("Trasportatore con id " + id + " non trovato in db"));
 
@@ -44,4 +46,24 @@ public class UserService {
             throw new BadRequestException(e.getMessage());
         }
     }
+
+    public Azienda putAziendaById(long id , AziendaDTO aziendaDTO) {
+        try {
+            Azienda azienda = aziendaRepository.findById(id).orElseThrow(()->new UserNotFoundException("Azienda con id " + id + " non trovata in db"));
+            azienda.setCap(aziendaDTO.cap());
+            azienda.setCitta(aziendaDTO.citta());
+            azienda.setRegione(aziendaDTO.regione());
+            azienda.setIndirizzo(aziendaDTO.indirizzo());
+            azienda.setEmail(aziendaDTO.email());
+            azienda.setNomeAzienda(aziendaDTO.nomeAzienda());
+            azienda.setFatturatoMedio(aziendaDTO.fatturatoMedio());
+            azienda.setNumeroDipendenti(aziendaDTO.numeroDipendenti());
+            azienda.setSettore(Settore.valueOf(aziendaDTO.settore()));
+            azienda.setPartitaIva(aziendaDTO.partitaIva());
+            return aziendaRepository.save(azienda);
+        }catch (Exception e){
+            throw new BadRequestException(e.getMessage());
+        }
+    }
+
     }
