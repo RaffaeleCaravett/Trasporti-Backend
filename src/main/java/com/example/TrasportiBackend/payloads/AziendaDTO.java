284939bc85e7970c6faf21c757495a1b96ff1ad5
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-26 21:51:52 +0200

    aziendaDto

---
+++ src/main/java/com/example/TrasportiBackend/payloads/AziendaDTO.java
@@ -0,0 +1,27 @@
+package com.example.TrasportiBackend.payloads;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record AziendaDTO (
+    @NotEmpty(message = "Nome azienda necessario.")
+    String nomeAzienda,
+    @NotEmpty(message = "Partita Iva necessario.")
+    String partitaIva,
+    @NotEmpty(message = "Fatturato medio necessario.")
+    String fatturatoMedio,
+    @NotNull(message = "Numero dipenddenti necessario.")
+    Long numeroDipendenti,
+    @NotEmpty(message = "Settore necessario.")
+    String settore,
+    @NotNull(message = "Cap necessario")
+    long cap,
+    @NotEmpty(message = "Email necessaria")
+    String email,
+    @NotEmpty(message = "Città necessaria")
+    String citta,
+    @NotEmpty(message = "Regione necessaria")
+    String regione,
+    @NotEmpty(message = "Indirizzo necessario")
+    String indirizzo
+    ){}
