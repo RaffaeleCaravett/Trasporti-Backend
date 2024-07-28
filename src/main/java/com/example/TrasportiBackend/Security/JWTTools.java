5322de641db73da797d62d05f76cc359555b9cf0
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-28 14:31:46 +0200

    done jwt tools

--- src/main/java/com/example/TrasportiBackend/Security/JWTTools.java
+++ src/main/java/com/example/TrasportiBackend/Security/JWTTools.java
@@ -1,4 +1,109 @@
 package com.example.TrasportiBackend.Security;
 
+import com.example.TrasportiBackend.User.*;
+import com.example.TrasportiBackend.exceptions.UnauthorizedException;
+import com.example.TrasportiBackend.payloads.entities.Tokens;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JWTTools {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
+    @Autowired
+    TrasporatoreRepository trasportatoreRepository;
+    @Autowired
+    AziendaRepository aziendaRepository;
+
+    public Tokens createTokens(User user){
+        String accessToken = Jwts.builder().setSubject(String.valueOf(user.getId()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*45))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes())).compact();
+
+        String refreshToken = Jwts.builder().setSubject(String.valueOf(user.getId()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60 *24 *7))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes())).compact();
+
+        Tokens token = new Tokens(accessToken,refreshToken);
+        return token;
+    }
+    public Trasportatore verifyTrasportatoreToken(String token){
+        try {
+            Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build().parse(token).getBody();
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            String userId = claims.getSubject();
+            return trasportatoreRepository.findById(Long.valueOf(userId)).get();
+        }catch (Exception e){
+            throw new UnauthorizedException("Il token non è valido! Per favore effettua nuovamente il login!");
+        }
+    }
+
+    public Azienda verifyAziendaToken(String token){
+        try {
+            Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build().parse(token).getBody();
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            String userId = claims.getSubject();
+            return aziendaRepository.findById(Long.parseLong(userId)).get();
+        }catch (Exception e){
+            throw new UnauthorizedException("Il token non è valido! Per favore effettua nuovamente il login!");
+        }
+    }
+
+
+    public Tokens verifyTrasportatoreRefreshToken(String token){
+        try {
+            Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build().parse(token).getBody();
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            String userId= claims.getSubject();
+
+            return  this.createTokens(trasportatoreRepository.findById(Long.valueOf(userId)).get());
+        }catch (Exception e){
+            throw new UnauthorizedException("Il refresh token non è valido. Accedi nuovamente.");
+        }
+    }
+    public Tokens verifyAziendaRefreshToken(String token){
+        try {
+            Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build().parse(token).getBody();
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            String userId= claims.getSubject();
+
+            return  this.createTokens(aziendaRepository.findById(Long.valueOf(userId)).get());
+        }catch (Exception e){
+            throw new UnauthorizedException("Il refresh token non è valido. Accedi nuovamente.");
+        }
+    }
+
+    public String extractIdFromToken(String token){
+        return  Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build().parseClaimsJws(token).getBody().getSubject();
+    }
 }
