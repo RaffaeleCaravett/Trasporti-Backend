4e073132ee6d1c3c4b729cddedd8f74c292063a3
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-29 13:15:57 +0200

    delete done too

--- src/main/java/com/example/TrasportiBackend/User/UserController.java
+++ src/main/java/com/example/TrasportiBackend/User/UserController.java
@@ -7,6 +7,7 @@ import com.example.TrasportiBackend.payloads.entities.TrasportatoreDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -43,18 +44,57 @@ public class UserController {
         return userService.getAllTrasportatori(page,size,orderBy);
     }
 
-    @PutMapping("/trasportatore/{id}")
-    public Trasportatore putById(@PathVariable long id,@RequestBody @Validated TrasportatoreDTO trasportatoreDTO, BindingResult bindingResult){
+    @PutMapping("/trasportatore/me")
+    @PreAuthorize("hasAuthority('Trasportatore')")
+    public Trasportatore putById(@AuthenticationPrincipal Trasportatore currentUser, @RequestBody @Validated TrasportatoreDTO trasportatoreDTO, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new BadRequestException(bindingResult.getAllErrors());
+        }
+        return userService.putTrasportatoreById(currentUser.getId(), trasportatoreDTO);
+    }
+    @PutMapping("/azienda/me")
+    @PreAuthorize("hasAuthority('Azienda')")
+    public Azienda putById(@AuthenticationPrincipal Azienda currentUser,@RequestBody @Validated AziendaDTO aziendaDTO, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new BadRequestException(bindingResult.getAllErrors());
+        }
+        return userService.putAziendaById(currentUser.getId(),aziendaDTO);
+    }
+    @PutMapping("/trasportatoreAdmin/{id}")
+    @PreAuthorize("hasAuthority('Admin')")
+    public Trasportatore putByIdAdmin(@PathVariable long id,@RequestBody @Validated TrasportatoreDTO trasportatoreDTO, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
             throw new BadRequestException(bindingResult.getAllErrors());
         }
         return userService.putTrasportatoreById(id,trasportatoreDTO);
     }
-    @PutMapping("/trasportatore/{id}")
-    public Azienda putById(@PathVariable long id,@RequestBody @Validated AziendaDTO aziendaDTO, BindingResult bindingResult){
+    @PutMapping("/aziendaAdmin/{id}")
+    @PreAuthorize("hasAuthority('Admin')")
+    public Azienda putByIdAdmin(@PathVariable long id,@RequestBody @Validated AziendaDTO aziendaDTO, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
             throw new BadRequestException(bindingResult.getAllErrors());
         }
         return userService.putAziendaById(id,aziendaDTO);
+    }
+
+    @DeleteMapping("/trasportatoreAdmin/{id}")
+    @PreAuthorize("hasAuthority('Admin')")
+    public boolean deleteByIdT(@PathVariable long id){
+        return userService.deleteTrasportatoreById(id);
+    }
+    @DeleteMapping("/aziendaAdmin/{id}")
+    @PreAuthorize("hasAuthority('Admin')")
+    public boolean deleteByIdA(@PathVariable long id){
+        return userService.deleteAziendaById(id);
+    }
+    @DeleteMapping("/trasportatore/me")
+    @PreAuthorize("hasAuthority('Trasportatore')")
+    public boolean deleteByIdT(@AuthenticationPrincipal Trasportatore trasportatore){
+        return userService.deleteTrasportatoreById(trasportatore.getId());
+    }
+    @DeleteMapping("/azienda/me")
+    @PreAuthorize("hasAuthority('Azienda')")
+    public boolean deleteByIdA(@AuthenticationPrincipal Azienda azienda){
+        return userService.deleteAziendaById(azienda.getId());
     }
 }
