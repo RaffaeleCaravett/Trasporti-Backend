e765040fb09e70dc3ecb2034d0da95aac4ea8478
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-31 21:32:20 +0200

    annuncio controller and annuncio service

---
+++ src/main/java/com/example/TrasportiBackend/Annuncio/AnnuncioService.java
@@ -0,0 +1,14 @@
+package com.example.TrasportiBackend.Annuncio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnnuncioService {
+
+    @Autowired
+    AnnuncioRepository annuncioRepository;
+
+
+    
+}
