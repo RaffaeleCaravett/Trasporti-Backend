f01ab2b2d70e622093c8bcc5f6f2fc690b498743
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-31 21:10:56 +0200

    done da and a between

--- src/main/java/com/example/TrasportiBackend/Spedizione/SpedizioneController.java
+++ src/main/java/com/example/TrasportiBackend/Spedizione/SpedizioneController.java
@@ -68,4 +68,17 @@ public class SpedizioneController {
     public Page<Spedizione> getByAziendaId(@PathVariable long aziendaId,@RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "10") int size,@RequestParam(defaultValue = "id") String orderBy){
         return spedizioneService.getAllByAziendaId(aziendaId,page,size,orderBy);
     }
+    @GetMapping("/byDa")
+    public Page<Spedizione> findByDa(@RequestParam(defaultValue = "") String da,@RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "10") int size,@RequestParam(defaultValue = "id") String orderBy){
+        return spedizioneService.findByDa(da,page,size,orderBy);
+    }
+    @GetMapping("/byA")
+    public Page<Spedizione> findByA(@RequestParam(defaultValue = "") String a,@RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "10") int size,@RequestParam(defaultValue = "id") String orderBy){
+        return spedizioneService.findByA(a,page,size,orderBy);
+    }
+    @GetMapping("/byDaAndA")
+    public Page<Spedizione> findByDaAndA(@RequestParam(defaultValue = "") String da,@RequestParam(defaultValue = "") String a,@RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "10") int size,@RequestParam(defaultValue = "id") String orderBy){
+        return spedizioneService.findByDaAndA(da,a,page,size,orderBy);
+    }
+
 }
