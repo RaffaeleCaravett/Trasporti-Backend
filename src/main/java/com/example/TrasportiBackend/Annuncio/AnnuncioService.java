4ef54e38c6413062e1fa1cb8656f5e6a2e2ce1de
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-01 21:24:13 +0200

    save annuncio in annuncio service

--- src/main/java/com/example/TrasportiBackend/Annuncio/AnnuncioService.java
+++ src/main/java/com/example/TrasportiBackend/Annuncio/AnnuncioService.java
@@ -1,5 +1,6 @@
 package com.example.TrasportiBackend.Annuncio;
 
+import com.example.TrasportiBackend.Spedizione.Spedizione;
 import com.example.TrasportiBackend.Spedizione.SpedizioneRepository;
 import com.example.TrasportiBackend.User.Azienda;
 import com.example.TrasportiBackend.User.AziendaRepository;
@@ -7,6 +8,8 @@ import com.example.TrasportiBackend.exceptions.UserNotFoundException;
 import com.example.TrasportiBackend.payloads.entities.AnnuncioDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class AnnuncioService {
@@ -22,7 +25,12 @@ public class AnnuncioService {
     public Annuncio save (AnnuncioDTO annuncioDTO){
         Annuncio annuncio = new Annuncio();
         Azienda azienda = aziendaRepository.findById(annuncioDTO.aziendaId()).orElseThrow(()->new UserNotFoundException("Azienda con id " + annuncioDTO.aziendaId() + " non trovato in db"));
-        
+        Spedizione spedizione = spedizioneRepository.findById(annuncioDTO.spedizioneId()).orElseThrow(()->new UserNotFoundException("Spedizione con id " + annuncioDTO.spedizioneId() + " non trovato in db." ));
+        annuncio.setAzienda(azienda);
+        annuncio.setSpedizione(spedizione);
+        annuncio.setDataPubblicazione(LocalDate.of(annuncioDTO.dataPubblicazioneAnno(),annuncioDTO.dataPubblicazioneMese(),annuncioDTO.dataPubblicazioneGiorno()));
+        annuncio.setRetribuzione(annuncioDTO.retribuzione());
+        return annuncioRepository.save(annuncio);
     }
 
 
--- src/main/java/com/example/TrasportiBackend/payloads/entities/AnnuncioDTO.java
+++ src/main/java/com/example/TrasportiBackend/payloads/entities/AnnuncioDTO.java
@@ -4,8 +4,14 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record AnnuncioDTO(
-        @NotEmpty(message = "Data pubblicazione necessaria")
-        String dataPubblicazione,
+        @NotNull(message = "Anno necessario")
+        int dataPubblicazioneAnno,
+        @NotNull(message = "Mese necessario")
+        int dataPubblicazioneMese,
+        @NotNull(message = "Giorno necessario")
+        int dataPubblicazioneGiorno,
+        @NotNull(message = "Retribuzione necessaria")
+        long retribuzione,
         @NotNull(message = "Azienda id necessario")
         long aziendaId,
         @NotNull(message = "Spedizione id necessario")
