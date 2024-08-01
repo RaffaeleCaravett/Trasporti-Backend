655c41ef8fec0215ad5503641593e94b71d14dc3
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-01 09:54:35 +0200

    now doing save method in annuncio service

--- src/main/java/com/example/TrasportiBackend/Annuncio/AnnuncioService.java
+++ src/main/java/com/example/TrasportiBackend/Annuncio/AnnuncioService.java
@@ -1,5 +1,10 @@
 package com.example.TrasportiBackend.Annuncio;
 
+import com.example.TrasportiBackend.Spedizione.SpedizioneRepository;
+import com.example.TrasportiBackend.User.Azienda;
+import com.example.TrasportiBackend.User.AziendaRepository;
+import com.example.TrasportiBackend.exceptions.UserNotFoundException;
+import com.example.TrasportiBackend.payloads.entities.AnnuncioDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +14,16 @@ public class AnnuncioService {
     @Autowired
     AnnuncioRepository annuncioRepository;
 
+    @Autowired
+    AziendaRepository aziendaRepository;
+    @Autowired
+    SpedizioneRepository spedizioneRepository;
+
+    public Annuncio save (AnnuncioDTO annuncioDTO){
+        Annuncio annuncio = new Annuncio();
+        Azienda azienda = aziendaRepository.findById(annuncioDTO.aziendaId()).orElseThrow(()->new UserNotFoundException("Azienda con id " + annuncioDTO.aziendaId() + " non trovato in db"));
+        
+    }
 
 
 }
