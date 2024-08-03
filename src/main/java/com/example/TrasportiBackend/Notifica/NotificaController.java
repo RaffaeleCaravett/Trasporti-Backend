adcced42437c351f6c9f311aac740d07701980e7
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-03 20:45:38 +0200

    done notifica controller

---
+++ src/main/java/com/example/TrasportiBackend/Notifica/NotificaController.java
@@ -0,0 +1,9 @@
+package com.example.TrasportiBackend.Notifica;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notifica")
+public class NotificaController {
+}
