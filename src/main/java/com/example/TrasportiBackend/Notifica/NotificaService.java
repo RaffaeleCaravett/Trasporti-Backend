dce0ac906cdc68012e11c3aa12bac6db07baefad
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-03 20:51:59 +0200

    done notifica DTO

--- src/main/java/com/example/TrasportiBackend/Notifica/NotificaService.java
+++ src/main/java/com/example/TrasportiBackend/Notifica/NotificaService.java
@@ -1,7 +1,15 @@
 package com.example.TrasportiBackend.Notifica;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificaService {
+    @Autowired
+    NotificaRepository notificaRepository;
+
+
+    public Notifica save(NotificaDTO notificaDTO){
+        
+    }
 }
---
+++ src/main/java/com/example/TrasportiBackend/payloads/entities/NotificaDTO.java
@@ -0,0 +1,15 @@
+package com.example.TrasportiBackend.payloads.entities;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record NotificaDTO(
+        @NotNull(message = "Spedizione id necessario")
+        long spedizioneId,
+        @NotNull(message = "Azienda id necessario")
+        long aziendaId,
+        @NotNull(message = "Trasportatore id necessario")
+        long trasportatoreId,
+        String statoNotifica
+) {
+}
