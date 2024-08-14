dfb53233cea01f786f73ca39cd497bea17dc3968
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-08-14 11:44:06 +0200

    done some extra methods

--- src/main/java/com/example/TrasportiBackend/Annuncio/AnnuncioController.java
+++ src/main/java/com/example/TrasportiBackend/Annuncio/AnnuncioController.java
@@ -3,6 +3,7 @@ package com.example.TrasportiBackend.Annuncio;
 import com.example.TrasportiBackend.exceptions.BadRequestException;
 import com.example.TrasportiBackend.payloads.entities.AnnuncioDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -41,6 +42,19 @@ public Annuncio save(@RequestBody @Validated AnnuncioDTO annuncioDTO, BindingRes
         throw new BadRequestException(bindingResult.getAllErrors());
     }
     return annuncioService.putById(annuncioDTO,aziendaId,annuncioId);
+    }
+
+    @GetMapping("/byAziendaId/{id}")
+    public Page<Annuncio> getByAziendaId(@PathVariable long id,@RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "10") int size,@RequestParam(defaultValue = "id") String orderBy){
+    return annuncioService.getByAziendaId(id,page,size,orderBy);
+    }
+    @GetMapping("/byRetribuzioneId/{retribuzione1}/{retribuzione2}")
+    public Page<Annuncio> getByRetribuzione(@PathVariable long retribuzione1,@PathVariable long retribuzione2,@RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "10") int size,@RequestParam(defaultValue = "id") String orderBy){
+        return annuncioService.findByRetribuzione(retribuzione1,retribuzione2,page,size,orderBy);
+    }
+    @GetMapping("/byData/{anno1}/{mese1}/{giorno1}/{anno2}/{mese2}/{giorno2}")
+    public Page<Annuncio> getByData(@PathVariable int anno1,@PathVariable int mese1,@PathVariable int giorno1,@PathVariable int anno2,@PathVariable int mese2,@PathVariable int giorno2,@RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "10") int size,@RequestParam(defaultValue = "id") String orderBy){
+        return annuncioService.findByData(anno1,mese1,giorno1,anno2,mese2,giorno2,page,size,orderBy);
     }
 
 }
