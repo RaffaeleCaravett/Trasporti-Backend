b8429c11921d6f3a15dd27d15139de5f9dad63a9
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-02 21:42:12 +0200

    created statistica service and injected in controller

--- src/main/java/com/example/TrasportiBackend/Statistiche/StatisticaController.java
+++ src/main/java/com/example/TrasportiBackend/Statistiche/StatisticaController.java
@@ -1,5 +1,6 @@
 package com.example.TrasportiBackend.Statistiche;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/azienda/statistica")
 public class StatisticaController {
+
+    @Autowired
+    StatisticaService statisticaService;
 
     @GetMapping("/{id}")
     @PreAuthorize("hasAnyAuthority('Admin','Azienda')")
---
+++ src/main/java/com/example/TrasportiBackend/Statistiche/StatisticaService.java
@@ -0,0 +1,10 @@
+package com.example.TrasportiBackend.Statistiche;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StatisticaService {
+@Autowired
+    StatisticaRepository statisticaRepository;
+}
