fdfc9f1f3c784da1a517f3201bf8acba9da3f425
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-02 21:44:22 +0200

    done a simple method that i need to change

--- src/main/java/com/example/TrasportiBackend/Statistiche/StatisticaService.java
+++ src/main/java/com/example/TrasportiBackend/Statistiche/StatisticaService.java
@@ -1,10 +1,16 @@
 package com.example.TrasportiBackend.Statistiche;
 
+import com.example.TrasportiBackend.exceptions.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StatisticaService {
-@Autowired
+    @Autowired
     StatisticaRepository statisticaRepository;
+
+
+    public Statistica getByAziendaId(long id){
+        return statisticaRepository.findByAzienda_Id(id).orElseThrow(() ->new BadRequestException("Non è stata trovata una statistica appartenente all'azienda con id " + id));
+    }
 }
