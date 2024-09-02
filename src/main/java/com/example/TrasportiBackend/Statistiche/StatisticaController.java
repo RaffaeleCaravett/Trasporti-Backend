186110303def6187bb475c5017c4c20b3855abd6
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-02 21:41:10 +0200

    created method get in controller

--- src/main/java/com/example/TrasportiBackend/Statistiche/StatisticaController.java
+++ src/main/java/com/example/TrasportiBackend/Statistiche/StatisticaController.java
@@ -1,4 +1,18 @@
 package com.example.TrasportiBackend.Statistiche;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/azienda/statistica")
 public class StatisticaController {
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('Admin','Azienda')")
+    public Statistica getByAziendaId(@PathVariable long id){
+        return statisticaService.getByAziendaId(id);
+    }
 }
