6bbbd93c07704a9e58773e8545b5312409eb320c
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-03 16:59:14 +0200

    created now recensione both for azienda and t

--- src/main/java/com/example/TrasportiBackend/Recensioni/RecensioneAz.java
+++ src/main/java/com/example/TrasportiBackend/Recensioni/RecensioneAz.java
@@ -1,4 +1,34 @@
 package com.example.TrasportiBackend.Recensioni;
 
+import com.example.TrasportiBackend.User.Azienda;
+import com.example.TrasportiBackend.User.Trasportatore;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "recensioni_az")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RecensioneAz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String commento;
+    private LocalDate localDate = LocalDate.now();
+    @ManyToOne
+    @JoinColumn(name = "azienda_id")
+    @JsonIgnore
+    private Azienda a;
+    @ManyToOne
+    @JoinColumn(name = "trasportatore_id")
+    @JsonIgnore
+    private Trasportatore da;
 }
