8d79017307abb9849f9bd9e40373056b71c4e9ad
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-04 21:48:05 +0200

    done methods in repository

--- src/main/java/com/example/TrasportiBackend/Recensioni/RecensioneAzRepository.java
+++ src/main/java/com/example/TrasportiBackend/Recensioni/RecensioneAzRepository.java
@@ -1,8 +1,13 @@
 package com.example.TrasportiBackend.Recensioni;
 
+import com.example.TrasportiBackend.enums.StatoNotifica;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecensioneAzRepository extends JpaRepository<RecensioneAz,Long> {
+
+    Page<RecensioneAz> findByA_IdAndStatoNotifica(long azId, StatoNotifica statoNotifica, Pageable pageable);
 }
--- src/main/java/com/example/TrasportiBackend/Recensioni/RecensioneTRepository.java
+++ src/main/java/com/example/TrasportiBackend/Recensioni/RecensioneTRepository.java
@@ -1,8 +1,13 @@
 package com.example.TrasportiBackend.Recensioni;
 
+import com.example.TrasportiBackend.enums.StatoNotifica;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecensioneTRepository extends JpaRepository<RecensioneT,Long> {
+    Page<RecensioneT> findByA_IdAndStatoNotifica(long tId, StatoNotifica statoNotifica, Pageable pageable);
+
 }
