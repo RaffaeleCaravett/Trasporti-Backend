710e58bc5cdd31ce6a779d971166910155794f6a
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-04 21:29:38 +0200

    done notifica recensione controller

--- src/main/java/com/example/TrasportiBackend/Notifica/NotificaRecensioniController.java
+++ src/main/java/com/example/TrasportiBackend/Notifica/NotificaRecensioniController.java
@@ -1,13 +1,30 @@
 package com.example.TrasportiBackend.Notifica;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/trasportatore/notificaRecensione")
 public class NotificaRecensioniController {
     @Autowired
     NotificaRecensioneService notificaRecensioneService;
+
+    @PostMapping("/leggi")
+    public boolean leggi (@RequestBody @Validated List<NotificaRecensione> notificaRecensiones){
+        return notificaRecensioneService.leggi(notificaRecensiones);
+    }
+    @GetMapping("TId/{id}")
+    public Page<NotificaRecensione> getByTId(@PathVariable long id){
+        return  notificaRecensioneService.getNotificheByTrasportatoreId(id);
+    }
+    @GetMapping("TIdAndStato/{id}/{stato}")
+    public List<NotificaRecensione> getByTIdAndStato(@PathVariable long id,@PathVariable String stato){
+        return  notificaRecensioneService.getNotificheByTrasportatoreIdAndStato(id,stato);
+    }
 }
 
