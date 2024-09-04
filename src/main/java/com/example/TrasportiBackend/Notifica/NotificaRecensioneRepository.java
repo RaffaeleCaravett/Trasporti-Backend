15e71c812c612acc6c6fb5848d28df2416be10ac
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-04 17:42:51 +0200

    ultimating notifica recensione

--- src/main/java/com/example/TrasportiBackend/Notifica/NotificaRecensioneRepository.java
+++ src/main/java/com/example/TrasportiBackend/Notifica/NotificaRecensioneRepository.java
@@ -1,8 +1,12 @@
 package com.example.TrasportiBackend.Notifica;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificaRecensioneRepository extends JpaRepository<NotificaRecensione,Long> {
+    Page<NotificaRecensione> findByA_Id(long aId, Pageable pageable);
+
 }
--- src/main/java/com/example/TrasportiBackend/Notifica/NotificaRecensioneService.java
+++ src/main/java/com/example/TrasportiBackend/Notifica/NotificaRecensioneService.java
@@ -1,10 +1,45 @@
 package com.example.TrasportiBackend.Notifica;
 
+import com.example.TrasportiBackend.User.TrasporatoreRepository;
+import com.example.TrasportiBackend.User.Trasportatore;
+import com.example.TrasportiBackend.enums.StatoNotifica;
+import com.example.TrasportiBackend.exceptions.BadRequestException;
+import com.example.TrasportiBackend.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class NotificaRecensioneService {
     @Autowired
     NotificaRecensioneRepository notificaRecensioneRepository;
+@Autowired
+    TrasporatoreRepository trasporatoreRepository;
+
+    public NotificaRecensione save (NotificaRecensione notificaRecensione){
+        return notificaRecensioneRepository.save(notificaRecensione);
+    }
+
+    public Page<NotificaRecensione> getNotificheByTrasportatoreId(long tId){
+        Trasportatore trasportatore = trasporatoreRepository.findById(tId).orElseThrow(()->new UserNotFoundException("Trasportatore con id " + tId + " non trovato in db."));
+        Pageable pageable = PageRequest.of(0,10, Sort.by("id"));
+        return notificaRecensioneRepository.findByA_Id(tId,pageable);
+    }
+    public boolean leggi(List<NotificaRecensione> recensiones){
+        try {
+            for (NotificaRecensione n : recensiones) {
+                n.setStatoNotifica(StatoNotifica.Letta);
+                notificaRecensioneRepository.save(n);
+            }
+            return true;
+        }catch (Exception e){
+            throw new BadRequestException(e.getMessage());
+        }
+    }
 }
