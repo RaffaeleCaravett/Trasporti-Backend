06db5fba68a1bf842499f40137cc28d26a696c91
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-04 09:19:31 +0200

    doing save recensione t

--- src/main/java/com/example/TrasportiBackend/Recensioni/RecensioneService.java
+++ src/main/java/com/example/TrasportiBackend/Recensioni/RecensioneService.java
@@ -1,5 +1,12 @@
 package com.example.TrasportiBackend.Recensioni;
 
+import com.example.TrasportiBackend.User.Azienda;
+import com.example.TrasportiBackend.User.AziendaRepository;
+import com.example.TrasportiBackend.User.TrasporatoreRepository;
+import com.example.TrasportiBackend.User.Trasportatore;
+import com.example.TrasportiBackend.exceptions.BadRequestException;
+import com.example.TrasportiBackend.exceptions.UserNotFoundException;
+import com.example.TrasportiBackend.payloads.entities.RecensioneTDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +16,19 @@ public class RecensioneService {
     RecensioneTRepository recensioneTRepository;
     @Autowired
     RecensioneAzRepository recensioneAzRepository;
+  @Autowired
+    AziendaRepository aziendaRepository;
+  @Autowired
+    TrasporatoreRepository trasporatoreRepository;
 
+    private RecensioneT saveRT(RecensioneTDTO recensioneTDTO){
 
-    private RecensioneT saveRT(){
-        throw new RuntimeException();
+        Azienda azienda = aziendaRepository.findById(recensioneTDTO.azienda_id()).orElseThrow(()->new UserNotFoundException("Azienda con id " + recensioneTDTO.azienda_id() + " non trovata in db."));
+        Trasportatore trasportatore = trasporatoreRepository.findById(recensioneTDTO.trasportatore_id()).orElseThrow(()->new UserNotFoundException("Trasportatore con id " + recensioneTDTO.trasportatore_id() + " non trovato in db."));
+
+        RecensioneT recensioneT = new RecensioneT();
+
+        recensioneT.setCommento(recensioneTDTO.message());
+        recensioneT.setDa();
     }
 }
