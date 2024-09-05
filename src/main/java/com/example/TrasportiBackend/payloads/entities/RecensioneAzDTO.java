ecfb044a93ff745d2c15720b019681714cac32f4
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-05 16:55:22 +0200

    improved receAzDto

--- src/main/java/com/example/TrasportiBackend/payloads/entities/RecensioneAzDTO.java
+++ src/main/java/com/example/TrasportiBackend/payloads/entities/RecensioneAzDTO.java
@@ -1,10 +1,15 @@
 package com.example.TrasportiBackend.payloads.entities;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public record RecensioneAzDTO(
         String message,
+        @NotNull(message = "Az_id necessario")
         long azienda_id,
-        long trasportatore_id
+        @NotNull(message = "T_id necessario")
+        long trasportatore_id,
+        @NotEmpty(message = "Polo necessario")
+        String polo
 ) {
 }
