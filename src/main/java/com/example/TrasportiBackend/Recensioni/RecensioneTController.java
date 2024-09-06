b1c12a3f6275b67efbcb32995e53cbcb3d57c9e3
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-06 16:14:42 +0200

    made save ReceT in rece t controller

--- src/main/java/com/example/TrasportiBackend/Recensioni/RecensioneTController.java
+++ src/main/java/com/example/TrasportiBackend/Recensioni/RecensioneTController.java
@@ -1,6 +1,13 @@
 package com.example.TrasportiBackend.Recensioni;
 
+import com.example.TrasportiBackend.exceptions.BadRequestException;
+import com.example.TrasportiBackend.payloads.entities.RecensioneAzDTO;
+import com.example.TrasportiBackend.payloads.entities.RecensioneTDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class RecensioneTController {
     @Autowired
     RecensioneService recensioneService;
+
+    @PostMapping("")
+    public RecensioneT save(@RequestBody @Validated RecensioneTDTO recensioneAzDTO, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new BadRequestException(bindingResult.getAllErrors());
+        }
+        return recensioneService.saveRT(recensioneAzDTO);
+    }
 }
