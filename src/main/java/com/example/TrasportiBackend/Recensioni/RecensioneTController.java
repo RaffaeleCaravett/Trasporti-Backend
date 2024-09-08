dc0e6030368248c4576f988fd98d1e65745ec26d
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-08 15:59:44 +0200

    authority in receTcontroller

--- src/main/java/com/example/TrasportiBackend/Recensioni/RecensioneTController.java
+++ src/main/java/com/example/TrasportiBackend/Recensioni/RecensioneTController.java
@@ -5,6 +5,7 @@ import com.example.TrasportiBackend.payloads.entities.RecensioneAzDTO;
 import com.example.TrasportiBackend.payloads.entities.RecensioneTDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ public class RecensioneTController {
     RecensioneService recensioneService;
 
     @PostMapping("")
+    @PreAuthorize("hasAuthority('Trasportatore')")
     public RecensioneT save(@RequestBody @Validated RecensioneTDTO recensioneAzDTO, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
             throw new BadRequestException(bindingResult.getAllErrors());
@@ -23,6 +25,7 @@ public class RecensioneTController {
         return recensioneService.saveRT(recensioneAzDTO);
     }
     @PutMapping("/{id}/{recensioneTDTOId}")
+    @PreAuthorize("hasAuthority('Trasportatore')")
     public RecensioneT putById(@PathVariable long id, @PathVariable long recensioneTDTOId, @RequestBody @Validated RecensioneTDTO recensioneAzDTO, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
             throw new BadRequestException(bindingResult.getAllErrors());
@@ -34,6 +37,7 @@ public class RecensioneTController {
         return recensioneService.getAllPaginatedT(page,size,orderBy,TId,stato);
     }
     @DeleteMapping("/{daId}/{receId}")
+    @PreAuthorize("hasAuthority('Trasportatore')")
     public boolean deleteByDaIdAndReceID (@PathVariable long daId, @PathVariable long receId){
         return recensioneService.deleteT(daId,receId);
     }
