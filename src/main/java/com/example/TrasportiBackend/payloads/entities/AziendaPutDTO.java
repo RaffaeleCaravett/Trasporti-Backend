b7327ce468ea42babf7f81e11c343b4ee4cc4330
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-15 17:02:34 +0200

    created aziendaPutDTO

---
+++ src/main/java/com/example/TrasportiBackend/payloads/entities/AziendaPutDTO.java
@@ -0,0 +1,27 @@
+package com.example.TrasportiBackend.payloads.entities;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record AziendaPutDTO(
+        @NotEmpty(message = "Nome azienda necessario.")
+        String nomeAzienda,
+        @NotEmpty(message = "Partita Iva necessario.")
+        String partitaIva,
+        @NotEmpty(message = "Fatturato medio necessario.")
+        String fatturatoMedio,
+        @NotNull(message = "Numero dipenddenti necessario.")
+        Long numeroDipendenti,
+        @NotEmpty(message = "Settore necessario.")
+        String settore,
+        @NotNull(message = "Cap necessario")
+        long cap,
+        @NotEmpty(message = "Email necessaria")
+        String email,
+        @NotEmpty(message = "Città necessaria")
+        String citta,
+        @NotEmpty(message = "Regione necessaria")
+        String regione,
+        @NotEmpty(message = "Indirizzo necessario")
+        String indirizzo
+){}
