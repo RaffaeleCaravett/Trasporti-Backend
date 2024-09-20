64914e68ae184a75450d8829bc81f569e7a5103b
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-20 21:51:44 +0200

    added find all method both in annuncio controller and service

--- src/main/java/com/example/TrasportiBackend/Annuncio/AnnuncioService.java
+++ src/main/java/com/example/TrasportiBackend/Annuncio/AnnuncioService.java
@@ -161,5 +161,8 @@ public Page<Annuncio> findByDaAndA(String da,String a,int page,int size, String 
                     Pageable pageable = PageRequest.of(page,size,Sort.by(Sort.Direction.fromString(direction),orderBy));
 return annuncioRepository.findBySpedizione_DaContainingIgnoreCaseAndSpedizione_AContainingIgnoreCase(da,a,pageable);
 }
-public Page<Annuncio> findAllPaginated()
+public Page<Annuncio> findAllPaginated(int page, int size, String orderBy, String direction){
+        Pageable pageable = PageRequest.of(page,size,Sort.by(Sort.Direction.fromString(direction),orderBy));
+        return annuncioRepository.findAll(pageable);
+}
 }
