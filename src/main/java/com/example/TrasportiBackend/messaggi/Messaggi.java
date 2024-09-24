efc88d34550e55bc5d060f212736c24a17cb176d
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-24 21:54:45 +0200

    created messaggi entity

--- src/main/java/com/example/TrasportiBackend/messaggi/Messaggi.java
+++ src/main/java/com/example/TrasportiBackend/messaggi/Messaggi.java
@@ -1,4 +1,28 @@
 package com.example.TrasportiBackend.messaggi;
 
+import com.example.TrasportiBackend.chat.Chat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "messaggi")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Messaggi {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    @JsonIgnore
+    private Chat chat;
+    private String sender;
+    private LocalDate createdAt;
 }
