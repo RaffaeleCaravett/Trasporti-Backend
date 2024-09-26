50a8a1812767c1bf0f1b94eb6b5b74e6afc8800e
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-26 21:53:13 +0200

    extended jpa repository in chat repository

--- src/main/java/com/example/TrasportiBackend/chat/ChatRepository.java
+++ src/main/java/com/example/TrasportiBackend/chat/ChatRepository.java
@@ -1,7 +1,8 @@
 package com.example.TrasportiBackend.chat;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ChatRepository {
+public interface ChatRepository extends JpaRepository<Chat,Long> {
 }
