7bc53a5743819d9a9e432bef2925a0c0e84b8db7
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-26 22:03:25 +0200

    made chat save method with chatDTO implementation in chat service

--- src/main/java/com/example/TrasportiBackend/chat/ChatService.java
+++ src/main/java/com/example/TrasportiBackend/chat/ChatService.java
@@ -2,6 +2,7 @@ package com.example.TrasportiBackend.chat;
 
 import com.example.TrasportiBackend.User.AziendaRepository;
 import com.example.TrasportiBackend.User.TrasporatoreRepository;
+import com.example.TrasportiBackend.exceptions.UserNotFoundException;
 import com.example.TrasportiBackend.payloads.entities.ChatDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,9 @@ public class ChatService {
     TrasporatoreRepository trasporatoreRepository;
     public Chat save(ChatDTO chatDTO){
         Chat chat = new Chat();
-        chat.
+        chat.setAzienda(aziendaRepository.findById(chatDTO.azienda_id()).orElseThrow(()-> new UserNotFoundException("Azienda con id "+ chatDTO.azienda_id() + " non trovata in db.")));
+        chat.setTrasportatore(trasporatoreRepository.findById(chatDTO.trasportatore_id()).orElseThrow(()-> new UserNotFoundException("Trasportatore con id "+ chatDTO.trasportatore_id() + " non trovato in db.")));
+
+        return chatRepository.save(chat);
     }
 }
