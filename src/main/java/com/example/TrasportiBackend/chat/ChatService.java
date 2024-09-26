00d103752ed7d3cdf86033003f552e7a8f5eb2d5
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-26 21:57:15 +0200

    injected chat repository

--- src/main/java/com/example/TrasportiBackend/chat/ChatService.java
+++ src/main/java/com/example/TrasportiBackend/chat/ChatService.java
@@ -1,7 +1,10 @@
 package com.example.TrasportiBackend.chat;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService {
+    @Autowired
+    ChatRepository chatRepository;
 }
