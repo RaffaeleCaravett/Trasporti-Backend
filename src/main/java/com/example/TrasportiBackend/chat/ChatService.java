2fa4e788553fbd5524a51174b4f6e16a90ff2296
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-26 22:00:40 +0200

    injected az repository in chat service

--- src/main/java/com/example/TrasportiBackend/chat/ChatService.java
+++ src/main/java/com/example/TrasportiBackend/chat/ChatService.java
@@ -1,5 +1,7 @@
 package com.example.TrasportiBackend.chat;
 
+import com.example.TrasportiBackend.User.AziendaRepository;
+import com.example.TrasportiBackend.payloads.entities.ChatDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,9 +9,11 @@ import org.springframework.stereotype.Service;
 public class ChatService {
     @Autowired
     ChatRepository chatRepository;
-
+@Autowired
+    AziendaRepository aziendaRepository;
 
     public Chat save(ChatDTO chatDTO){
         Chat chat = new Chat();
+        chat.
     }
 }
