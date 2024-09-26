d7d7fb4589b7f4f7b9e049a11b11aceb038c0104
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-26 21:56:08 +0200

    created a custom method in repository

--- src/main/java/com/example/TrasportiBackend/chat/ChatRepository.java
+++ src/main/java/com/example/TrasportiBackend/chat/ChatRepository.java
@@ -3,6 +3,9 @@ package com.example.TrasportiBackend.chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatRepository extends JpaRepository<Chat,Long> {
+    List<Chat> findByAzienda_IdAndTrasportatore_Id(long azId,long trId);
 }
