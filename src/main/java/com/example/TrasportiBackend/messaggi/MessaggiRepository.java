6626d32173c31070ea3d164997a639d814e3fd51
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-29 17:15:09 +0200

    created messaggi repository

---
+++ src/main/java/com/example/TrasportiBackend/messaggi/MessaggiRepository.java
@@ -0,0 +1,8 @@
+package com.example.TrasportiBackend.messaggi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessaggiRepository extends JpaRepository<Messaggi,Long> {
+}
