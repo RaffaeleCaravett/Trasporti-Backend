05ff6f877eb90edd263edb39551f869aba391fd7
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-29 17:06:35 +0200

    created az controller

--- src/main/java/com/example/TrasportiBackend/chat/ChatAzController.java
+++ src/main/java/com/example/TrasportiBackend/chat/ChatAzController.java
@@ -1,9 +1,37 @@
 package com.example.TrasportiBackend.chat;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.TrasportiBackend.exceptions.DtoHasErrors;
+import com.example.TrasportiBackend.exceptions.UserNotFoundException;
+import com.example.TrasportiBackend.payloads.entities.ChatDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/azienda/chat")
 public class ChatAzController {
+    @Autowired
+    ChatService chatService;
+    @PostMapping("")
+    @PreAuthorize("hasAuthority('Azienda')")
+    public Chat save (@RequestBody @Validated ChatDTO chatDTO, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new DtoHasErrors(bindingResult.getAllErrors());
+        }
+        return chatService.save(chatDTO);
+    }
+    @GetMapping("/byTId/{id}")
+    @PreAuthorize("hasAuthority('Azienda')")
+    public List<Chat> getByTId(@PathVariable long id){
+        return chatService.getByTrasportatoreId(id);
+    }
+    @GetMapping("/byAziendaIdAndTrasportatoreId/{aziendaId}/{trasportatoreId}")
+    @PreAuthorize("hasAuthority('Azienda')")
+    public Chat findByAzIdAndTId(@PathVariable long aziendaId,@PathVariable long trasportatoreId){
+        return chatService.getByAziendaIdAndTrasportatoreId(aziendaId,trasportatoreId).orElseThrow(()-> new UserNotFoundException("Chat non trovata"));
+    }
 }
