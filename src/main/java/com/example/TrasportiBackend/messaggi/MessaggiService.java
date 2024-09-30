8a79618fb47e2eca4c94a8efdcf1c9f9802e4c87
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-30 21:11:31 +0200

    injected message Repository into message service

--- src/main/java/com/example/TrasportiBackend/messaggi/MessaggiService.java
+++ src/main/java/com/example/TrasportiBackend/messaggi/MessaggiService.java
@@ -1,7 +1,10 @@
 package com.example.TrasportiBackend.messaggi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessaggiService {
+    @Autowired
+    MessaggiRepository messaggiRepository;
 }
