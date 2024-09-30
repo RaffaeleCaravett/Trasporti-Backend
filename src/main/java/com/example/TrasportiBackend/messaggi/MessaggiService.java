8f7bbbf60b82d1aa06e49c904e8c8964b0c296cc
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-30 21:58:02 +0200

    doing messaggi service

--- src/main/java/com/example/TrasportiBackend/messaggi/MessaggiService.java
+++ src/main/java/com/example/TrasportiBackend/messaggi/MessaggiService.java
@@ -1,9 +1,12 @@
 package com.example.TrasportiBackend.messaggi;
 
+import com.example.TrasportiBackend.User.Azienda;
 import com.example.TrasportiBackend.User.AziendaRepository;
 import com.example.TrasportiBackend.User.TrasporatoreRepository;
 import com.example.TrasportiBackend.User.Trasportatore;
+import com.example.TrasportiBackend.chat.Chat;
 import com.example.TrasportiBackend.chat.ChatRepository;
+import com.example.TrasportiBackend.exceptions.UserNotFoundException;
 import com.example.TrasportiBackend.payloads.entities.MessaggioDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +22,9 @@ public class MessaggiService {
 @Autowired
     TrasporatoreRepository trasporatoreRepository;
     public Messaggi save(MessaggioDTO messaggioDTO){
+        Chat chat = chatRepository.findById(messaggioDTO.chat_id()).orElseThrow(()-> new UserNotFoundException("Chat non trovata in db."));
+        Azienda azienda = new Azienda();
+        Trasportatore trasportatore = new Trasportatore();
 
     }
 }
