1ae9959c2e98fb679863574ace300efd05b595ed
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-30 21:55:09 +0200

    injected chat repo into messaggi service

--- src/main/java/com/example/TrasportiBackend/messaggi/MessaggiService.java
+++ src/main/java/com/example/TrasportiBackend/messaggi/MessaggiService.java
@@ -1,5 +1,7 @@
 package com.example.TrasportiBackend.messaggi;
 
+import com.example.TrasportiBackend.chat.ChatRepository;
+import com.example.TrasportiBackend.payloads.entities.MessaggioDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 public class MessaggiService {
     @Autowired
     MessaggiRepository messaggiRepository;
+@Autowired
+    ChatRepository chatRepository;
 
 
     public Messaggi save(MessaggioDTO messaggioDTO){
