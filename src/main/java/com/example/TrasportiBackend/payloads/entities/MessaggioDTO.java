329774262cf2a89f17966ecc40adf2fd5d20e41d
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-09-30 21:52:10 +0200

    ultimated messaggioDTO

--- src/main/java/com/example/TrasportiBackend/payloads/entities/MessaggioDTO.java
+++ src/main/java/com/example/TrasportiBackend/payloads/entities/MessaggioDTO.java
@@ -1,5 +1,6 @@
 package com.example.TrasportiBackend.payloads.entities;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record MessaggioDTO(
@@ -8,6 +9,10 @@ public record MessaggioDTO(
         @NotNull(message = "Sender_id necessario")
         long sender_id,
         @NotNull(message = "Receiver_id necessario")
-        long receiver_id
+        long receiver_id,
+        @NotEmpty(message = "receiver_type necessario")
+        String receiverType,
+        @NotEmpty(message = "sender_type necessario")
+        String senderType
 ) {
 }
