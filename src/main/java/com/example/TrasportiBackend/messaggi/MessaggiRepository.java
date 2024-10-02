a10c24251feee74e3989b3b041346be1b9451850
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-10-02 21:50:08 +0200

    done find by chat id in messaggi repo

--- src/main/java/com/example/TrasportiBackend/messaggi/MessaggiRepository.java
+++ src/main/java/com/example/TrasportiBackend/messaggi/MessaggiRepository.java
@@ -3,7 +3,9 @@ package com.example.TrasportiBackend.messaggi;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessaggiRepository extends JpaRepository<Messaggi,Long> {
-
+List<Messaggi> findByChat_Id(long chatId);
 }
