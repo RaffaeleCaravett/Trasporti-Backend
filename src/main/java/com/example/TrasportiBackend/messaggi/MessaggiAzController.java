3f28a9613917fdfdd4d3fe9bc0162e83bea4da8e
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-10-07 08:54:07 +0200

    doing messaggi controller

--- src/main/java/com/example/TrasportiBackend/messaggi/MessaggiAzController.java
+++ src/main/java/com/example/TrasportiBackend/messaggi/MessaggiAzController.java
@@ -1,9 +1,27 @@
 package com.example.TrasportiBackend.messaggi;
 
+import com.example.TrasportiBackend.payloads.entities.MessaggioDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/azienda/messaggi")
 public class MessaggiAzController {
+
+    @Autowired
+    private MessaggiService messaggiService;
+
+
+
+    @PostMapping("")
+    public Messaggi save(@RequestBody @Validated MessaggioDTO messaggioDTO, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            
+        }
+    }
 }
