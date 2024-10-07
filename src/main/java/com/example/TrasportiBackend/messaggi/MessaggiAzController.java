6119010ccce47f4583f5627ee58f4ee85a6912ef
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-10-07 09:00:10 +0200

    done post, get, put and now to do delete

--- src/main/java/com/example/TrasportiBackend/messaggi/MessaggiAzController.java
+++ src/main/java/com/example/TrasportiBackend/messaggi/MessaggiAzController.java
@@ -1,13 +1,14 @@
 package com.example.TrasportiBackend.messaggi;
 
+import com.example.TrasportiBackend.exceptions.DtoHasErrors;
 import com.example.TrasportiBackend.payloads.entities.MessaggioDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/azienda/messaggi")
@@ -19,9 +20,22 @@ public class MessaggiAzController {
 
 
     @PostMapping("")
+    @PreAuthorize("hasAuthority('Azienda')")
     public Messaggi save(@RequestBody @Validated MessaggioDTO messaggioDTO, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
-            
+            throw new DtoHasErrors(bindingResult.getAllErrors());
         }
+        return messaggiService.save(messaggioDTO);
     }
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('Azienda')")
+    public List<Messaggi> getByChatId(@PathVariable long id){
+        return messaggiService.getByChatId(id);
+    }
+    @PutMapping("/{uId}/{messaggioId}")
+    @PreAuthorize("hasAuthority('Azienda')")
+    public Messaggi putById(@RequestBody @Validated MessaggioDTO messaggioDTO,BindingResult bindingResult, @PathVariable long uId,@PathVariable long messaggioId){
+        return messaggiService.putById(messaggioId,uId,"Azienda",messaggioDTO);
+    }
+    
 }
