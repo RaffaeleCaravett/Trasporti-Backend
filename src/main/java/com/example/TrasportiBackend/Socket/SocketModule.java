3b6aa6dc1314535a5769418ac50cdb86b76ee348
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-10-26 15:47:27 +0200

    doing socket

--- src/main/java/com/example/TrasportiBackend/Socket/SocketModule.java
+++ src/main/java/com/example/TrasportiBackend/Socket/SocketModule.java
@@ -10,6 +10,7 @@ import com.example.TrasportiBackend.payloads.entities.MessaggioDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -42,10 +43,15 @@ public class SocketModule {
     private ConnectListener onConnected() {
         return (client) -> {
             var params = client.getHandshakeData().getUrlParams();
-            String room = params.get("room").stream().collect(Collectors.joining());
-            String username = params.get("username").stream().collect(Collectors.joining());
-            client.joinRoom(room);
-            log.info("Socket ID[{}] - room[{}] - username [{}]  Connected to chat module through", client.getSessionId().toString(), room, username);
+            List<String> room=params.get("room");
+            List<String> username=params.get("username");
+            if(username!=null) {
+                username.stream().collect(Collectors.joining());
+            }
+            if(room!=null) {
+                room.stream().collect(Collectors.joining());
+                client.joinRoom(room.toString());
+            }
         }
         ;
 
@@ -54,10 +60,15 @@ public class SocketModule {
     private DisconnectListener onDisconnected() {
         return client -> {
             var params = client.getHandshakeData().getUrlParams();
-            String room = params.get("room").stream().collect(Collectors.joining());
-            String username = params.get("username").stream().collect(Collectors.joining());
-            log.info("Socket ID[{}] - room[{}] - username [{}]  discnnected to chat module through", client.getSessionId().toString(), room, username);
-        }
+            List<String> room=params.get("room");
+            List<String> username=params.get("username");
+            if(room!=null) {
+                room.stream().collect(Collectors.joining());
+            }
+            if(username!=null) {
+                username.stream().collect(Collectors.joining());
+            }
+            }
         ;
     }
 }
