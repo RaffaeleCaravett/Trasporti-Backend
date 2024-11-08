3619e3e6eb8dd066c6c1e187d66974c295ab6acf
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-11-08 14:50:06 +0100

    enlarged token duration and succesfuly generated jasper report

--- src/main/java/com/example/TrasportiBackend/Security/JWTTools.java
+++ src/main/java/com/example/TrasportiBackend/Security/JWTTools.java
@@ -27,7 +27,7 @@ public class JWTTools {
     public Tokens createTokens(User user){
         String accessToken = Jwts.builder().setSubject(String.valueOf(user.getId()))
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*45))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*2))
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes())).compact();
 
         String refreshToken = Jwts.builder().setSubject(String.valueOf(user.getId()))
